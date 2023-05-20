f25944d8c12b96e8781815012416a0d800574e89
Julius L <116513648+Julius873@users.noreply.github.com>
2023-05-20 19:07:11 -0400

    Update app.js
    More edits

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello, World! Thank you for coming. Nice meeting you. I have added the requested line'); 
+res.send('Hello, World! Thank you for coming. Nice meeting you. I have added the requested line. We will make additional changes using Jenkins'); 
 const port = 3000;
 app.listen(port, () => {
 console.log(`Server running on port ${port}`);
